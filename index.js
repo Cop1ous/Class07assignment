3c1330bb6a724669e555b3c4938cda08156f6b4a
Cop1ous <85597066+Cop1ous@users.noreply.github.com>
2023-10-19 15:14:31 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,83 @@
+function maximum(smallestNumber, middleNumber, largestNumber){
+maxNumberOne =(12, 18, 3)
+console.log(maxNumberOne) //max number is 18
+maxNumberTwo =(100, 4, 20)
+console.log(maxNumberTwo) //max number is 100
+maxNumberThree =(24, 41, 87)
+console.log(maxNumberThree) //max number is 87
+}
+Math.max(maximum);
+
+//Question 2------------------
+function diceRoll(){
+    Math.random(min=1, max=6)
+}
+//Thi chooses a random number between 1 and 6
+function winner(four, notFour){
+    if (
+        diceRoll == 4
+    )
+        console.log("Winner")
+    else (
+        diceRoll !== 4
+    )
+    console.log("You lost");
+}
+//This will find is the rolled number was 4, and declare wether you won based on your roll or not
+number = diceRoll
+
+//Question 3-------------------
+function fibonacci(number){
+    if(
+        number == 0
+    )
+    console.log(number);
+    else(
+        number == 1
+    )
+    return(number);
+    return(fibonacci(number-1)+fibonacci(number-2));
+}
+count(fibonacci) = 6;
+//Question 4------------------
+function capitalise (location){
+    return(capital);
+}
+function checkLocation(location){
+    location = capitalise
+if(
+    location === Vancouver
+)
+console.log("It is rainy here")
+else(
+    location = capitalise
+)
+locationFormatted
+console.log(locationFormatted)
+}
+
+//Question 5---------------------
+
+
+//Question 5---------------------
+/**
+ *Make a function that will generate the random radius of a circle, and use the randomly generated radius to calculate the 
+ * area of that circle. remember the formula to calculate a circle is PiR^2. Then use that circle to calculate the area of a cylinder with a height of 5. Note your code.
+ */
+function circleArea(radius){
+  radius=Math.random(min=-5, max=10); //this will geerate a random number for the radius
+    if(
+        radius > 0
+    ) //This will find if the radius is a positive number, as you cant really have negative radius circles
+    {Square = radius*radius; //This will calculate the square of the radius
+    Square.toFixed(2)
+    Area=(Math.PI*Square)} //This will do the math to calculte the area nd return the result
+    return(Area);
+    return 0; //This will give you 0 if the circle radius is 0 or less than 0
+
+    function cylinder(){
+        
+    }
+}
+
+
